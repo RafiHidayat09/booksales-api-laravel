a0874f0b379bb58ce2d780a9bd67b256b6d835b7
RafiHidayat09 <mrafihidayat21@gmail.com>
2025-10-15 18:28:42 +0700

    commit tugas 3 Laravel

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -7,10 +7,18 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-     public function index(){
-        $authors = Author::all(); // Supaya mengambil semua data dan kirim ke view
-        return view('authors', ['authors' => $authors]);
+      public function index(){
+        $authors = Author::all();
+        return response()->json([
+            "success" => true,
+            "massage" => "Get All Resources",
+            "data" => $authors
+        ], 200);
     }
+    //  public function index(){
+    //     $authors = Author::all(); // Supaya mengambil semua data dan kirim ke view
+    //     return view('authors', ['authors' => $authors]);
+    // }
     //   public function index (){
     //    $data = new Author(); // Membuat object
     //    $authors = $data->getAuthors(); // Mengakses method getAuthor
--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,11 +7,18 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    
     public function index(){
-        $books = Book::all(); // Supaya mengambil semua data dan kirim ke view
-        return view('books', ['books' => $books]);
+        $books = Book::all();
+        return response()->json([
+            "success" => true,
+            "massage" => "Get All Resources",
+            "data" => $books
+        ], 200);
     }
+    // public function index(){
+    //     $books = Book::all(); // Supaya mengambil semua data dan kirim ke view
+    //     return view('books', ['books' => $books]);
+    // }
     // public function index(){
     //    $data = new Book(); // Membuat object
     //    $books = $data->getBooks(); // Mengakses method getBooks
